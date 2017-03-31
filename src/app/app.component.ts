d052b3df7d94d71c57b0330bc97e165056eef9ea
Katie Reed <katharynreedpierce@gmail.com>
2017-03-31 09:32:50 -0700

    create food model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,17 @@ export class AppComponent {
     new Food ('Big Mac', 'I wish it were a Bigger Mac', 540);
     new Food ('McFries', 'I only got a medium #sad', 330);
     new Food ('Hi-C Orange Lavaburst', 'it was just so orange', 220);
-  ]
+  ];
+
+  showNewFood() {
+    this.newFood = true;
+  }
+
+  editFood(food) {
+    this.selectedFood = food;
+  }
+
+  finishedEditing() {
+    this.selectedFood = null;
+  }
 }
