50ba214f681a078eacda51846a416017fdaa7454
Katie Reed <katharynreedpierce@gmail.com>
2017-03-31 09:50:57 -0700

    create initial functionality to food-detail component

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -8,7 +8,20 @@ import { AppComponent } from '../app.component';
   styleUrls: ['./food-detail.component.css']
 })
 export class FoodDetailComponent {
+  @Input() childFoodList: Food[];
+  @Output() editClicker = new EventEmitter();
 
+  editFoodHasBeenClicked(foodToEdit: Food) {
+    this.editClicker.emit(foodToEdit);
+  }
+
+  calorieFiltration(food) {
+    if (food.calories < 500) {
+      return "bg-success";
+    } else if (food.calories > 500) {
+      return "bg-danger";
+    }
+  }
 
 
 }
