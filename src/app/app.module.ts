ca11598e7d2bce7e32bfd2d0d769b2b0168c8bfc
Katie Reed <katharynreedpierce@gmail.com>
2017-03-31 09:33:34 -0700

    create new-food component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NewFoodComponent } from './new-food/new-food.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFoodComponent
   ],
   imports: [
     BrowserModule,
