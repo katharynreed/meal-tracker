5e89499a6adaf45f090541b981199d865db90a71
Katie Reed <katharynreedpierce@gmail.com>
2017-03-31 08:51:19 -0700

    begin initial build for food class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  newFood = false;
+  selectedFood = null;
+
+  masterFoods: Food[] = [
+    new Food ('Big Mac', 'I wish it were a Bigger Mac', 540);
+    new Food ('McFries', 'I only got a medium #sad', 330);
+    new Food ('Hi-C Orange Lavaburst', 'it was just so orange', 220);
+  ]
 }
